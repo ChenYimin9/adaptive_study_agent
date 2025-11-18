147b4b3fd7a9ba7e13f5f41ba4a258cc4304fef1
Chenyimin <15989437470@163.com>
2025-11-18 23:04:20 +0800

    q

--- core/data_manager.py
+++ core/data_manager.py
@@ -11,7 +11,7 @@ class Config:
     POOL_CONFIG = {
         'host': os.environ.get('MYSQLHOST', 'localhost'),
         'user': os.environ.get('MYSQLUSER', 'root'),
-        'password': os.environ.get('MYSQLPASSWORD', '123456'),
+        'password': os.environ.get('MYSQLPASSWORD', 'TbmMjnfScHMmjVuLyGGEWbENvudftkPt'),
         'database': os.environ.get('MYSQLDATABASE', 'railway'),
         'port': int(os.environ.get('MYSQLPORT', 3306)),
         'maxconnections': 5,  # 最大连接数
